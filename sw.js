cf347b324c763d9bf82de35efb7b521a1347d3c0
rifkiachmads <rifkiachmads@gmail.com>
2020-12-05 02:44:38 +0700

    v1

---
+++ sw.js
@@ -0,0 +1,18 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.0/workbox-sw.js');
+
+
+workbox.precaching.precacheAndRoute([
+    { url: '/index.html', revision: null },
+    { url: '/007 James Bond Theme.mp3', revision: null },
+    { url: '/Ba Dum Tss!.mp3', revision: null },
+    { url: '/Surprised (Sound Effects).mp3', revision: null },
+], 
+{
+    // Ignore all URL parameters.
+    ignoreURLParametersMatching: [/.*/]
+});
+  
+workbox.routing.registerRoute(
+  () => '/',
+  new workbox.strategies.StaleWhileRevalidate()
+);
