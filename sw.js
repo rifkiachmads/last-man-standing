225205cd34b655b4726d14bbd3d281275dc9866c
rifkiachmads <rifkiachmads@gmail.com>
2020-12-05 02:54:35 +0700

    remove prefix '/' from any url

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.0/workbox
 
 
 workbox.precaching.precacheAndRoute([
-    { url: '/index.html', revision: null },
-    { url: '/007 James Bond Theme.mp3', revision: null },
-    { url: '/Ba Dum Tss!.mp3', revision: null },
-    { url: '/Surprised (Sound Effects).mp3', revision: null },
+    { url: 'index.html', revision: null },
+    { url: '007 James Bond Theme.mp3', revision: null },
+    { url: 'Ba Dum Tss!.mp3', revision: null },
+    { url: 'Surprised (Sound Effects).mp3', revision: null },
 ], 
 {
     // Ignore all URL parameters.
