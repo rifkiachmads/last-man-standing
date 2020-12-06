6478b4e9f2572809938ab47d03a80eafc478f50e
rifkiachmads <rifkiachmads@gmail.com>
2020-12-06 21:38:15 +0700

    Temporarily permit precache video in SW (still get error 206)

--- sw.js
+++ sw.js
@@ -4,23 +4,36 @@ workbox.setConfig({ debug: true });
 
 workbox.precaching.precacheAndRoute([
     { url: 'index.html', revision: null },
-    { url: '007 James Bond Theme.mp3', revision: null },
-    { url: 'Ba Dum Tss!.mp3', revision: null },
-    { url: 'Surprised (Sound Effects).mp3', revision: null },
-], 
-{
-    // Ignore all URL parameters.
-    ignoreURLParametersMatching: [/.*/]
+]);
+
+// doesnt work because of the 206 stuff :(
+const cacheName = 'last-man-standing-media-cache';
+const cacheURLs = [
+  'Mission Impossible Theme Song (Flute Cover).mp3',
+  'Ba Dum Tss!.mp3',
+  'Surprised (Sound Effects).mp3',
+]
+self.addEventListener('install', (event) => {
+  const precache = async () => {
+    const cache = await caches.open(cacheName);
+    await Promise.all([
+      ...cacheURLs.map(url => cache.add(url))
+    ])
+  };
+  event.waitUntil(precache());
 });
   
+
 workbox.routing.registerRoute(
-  () => '/',
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'last-man-standing-cache',
+  ({url}) => url.pathname.endsWith('.mp3'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'last-man-standing-media-cache',
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxAgeSeconds: 24 * 60 * 60 * 365,
       }),
+      new workbox.cacheableResponse.CacheableResponsePlugin({statuses: [200]}),
+      new workbox.rangeRequests.RangeRequestsPlugin(),
     ],
   })
 );
